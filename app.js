4f691ed4bc388208adc014b5155cfcb9d8915c4e
Luke Pighetti <lukepighetti@gmail.com>
2018-05-17 18:36:40 +0000

    about to change BlockNotify from a class into a function

--- app.js
+++ app.js
@@ -4,4 +4,33 @@ const firebaseConfig = require('./config/serviceAccountKey.json')
 const axios = require('axios')
 const admin = require('firebase-admin')
 
+const BlockNotify = require('./lib/BlockNotify.js')
+const getBlockchain = require('./lib/getBlockchain.js')
+
+
+//  listen for new blocks
+//    get blockchain data
+//    push it to latestData
+
+new BlockNotify(async (blockHash) => {
+  console.log(`blockNotify! ${blockHash}`)
+  const resultBlockchain = await getBlockchain(blockHash)
+  console.log(`resultBlockchain`)
+  console.log(resultBlockchain)
+})
+
+
+//  wait 15 seconds
+//    get CoinGecko data
+//    get Pool data
+//    push it to latestData
+
+
+//  listen for update to latestData.chain
+//    add to rollingAverage
+//    if height % 84 == 0
+//      save rollingAverage to averageHistory
+
+
+
 console.log('it works!')
