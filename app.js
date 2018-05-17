90b309f90995db542f843c8c4541e507104e9ecf
Luke Pighetti <lukepighetti@gmail.com>
2018-05-17 17:54:54 +0000

    added axios and firebase admin

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+const config = require('./config/config.json')
+const firebaseConfig = require('./config/serviceAccountKey.json')
+
+const axios = require('axios')
+const admin = require('firebase-admin')
+
 console.log('it works!')
